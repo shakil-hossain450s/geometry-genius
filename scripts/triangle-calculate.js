53b386006c265b1bf6809827329a7a02ba21a9c9
md shakil hossain <shakil.hossain450s@gmail.com>
2024-08-24 10:45:00 +0600

    triangle area done

---
+++ scripts/triangle-calculate.js
@@ -0,0 +1,17 @@
+function calculateTriangle(){
+    // get the base value
+    const triangleBaseInput = document.getElementById('triangle-base');
+    const triangleBaseText = triangleBaseInput.value;
+    const base = parseFloat(triangleBaseText);
+    
+    // get the height value
+    const triangleHeightInput = document.getElementById('triangle-height');
+    const triangleHeightText = triangleHeightInput.value;
+    const height = parseFloat(triangleHeightText);
+
+    const area = 0.5 * base * height;
+
+    const triangleArea = document.getElementById('triangle-area');
+    triangleArea.innerText = area;
+    console.log('area of the triangle is:', area);
+}
